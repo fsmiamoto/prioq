2ad72cec66db90acacb50204d4e12be4ca8d497f
Francisco Miamoto <fsmiamoto@gmail.com>
2021-12-27 18:54:46 -0300

    add items example

---
+++ example_items_test.go
@@ -0,0 +1,46 @@
+package prioq_test
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/fsmiamoto/prioq"
+)
+
+// Item is an example defined type
+type Item struct {
+	key int
+}
+
+// MaxItem is an example CompareFunc that builds a MaxHeap using the key property
+func MaxItem(node, child Item) bool {
+	return child.key > node.key
+}
+
+// This example shows how to implement your own CompareFunc and the use on
+// a defined type
+func Example_items() {
+	values := []Item{
+		Item{key: 8},
+		Item{key: 22},
+		Item{key: 3},
+		Item{key: 14},
+		Item{key: 22},
+	}
+
+	pq := prioq.NewWithCompareFunc[Item](values, MaxItem)
+
+	for !pq.IsEmpty() {
+		item, err := pq.Extract()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(item.key)
+	}
+	// Output:
+	// 22
+	// 22
+	// 14
+	// 8
+	// 3
+}
